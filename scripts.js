b64ca39d252c95405c0d3da065a6d6ded76d924c
Zeyad Abdullatif Abdullwahab Korkor <zayad30982025@f-eng.tanta.edu.eg>
2024-02-22 21:39:20 +0200

    Refactor event handling in scripts.js

--- scripts.js
+++ scripts.js
@@ -1,21 +1,12 @@
-console.log("123");
+ 
+ const questionsLabels = document.querySelectorAll(".faq__questions");
 
-// const questionsLabels = document.querySelectorAll(".faq__questions");
+const onClickHandler = (item) => {
+  item.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") item.click();
+  });
+};
+questionsLabels.forEach(onClickHandler)
+ 
 
-// const onClickHandler = (item) => {
-//   item.addEventListener("keydown", (event) => {
-//     if (event === "Enter") item.click();
-//   });
-// };
-// questionsLabels.forEach(onClickHandler);
-
-
-// const questionsLabels = document.querySelectorAll(".faq__questions");
-
-// const onClickHandler = (item) => {
-//   item.addEventListener("keydown", (event) => {
-//     if (event.key === "Enter") item.click();
-//   });
-// };
-
-// questionsLabels.forEach(onClickHandler);
+ 
